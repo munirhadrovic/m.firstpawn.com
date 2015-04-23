5f210665cb4812edb91d3d303c8360feefee4253
anes89cok <anes89cok@gmail.com>
2015-04-23 10:59:49 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="889 Airport Road S."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples, FL 34104";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica="FL"; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2394347296 ;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
